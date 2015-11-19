d88c97e9b427d841deb0257db3ad274ac6faac09
Waddafac <castellini.davide19@gmail.com>
2015-11-19 16:54:20 +0100

    initial commit with source

---
+++ core/src/com/mygdx/game/Player.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Player {
+    int x = 120, y = 120;
+    Texture img = new Texture("sprites/testChar2.png");
+
+}
