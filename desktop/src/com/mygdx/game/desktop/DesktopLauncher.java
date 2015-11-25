ddd248f1729b2eb3e61d972f573dfa85ded796c0
Waddafac <castellini.davide19@gmail.com>
2015-11-25 11:54:31 +0100

    Added classes

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,16 +2,16 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Team3;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "platformGame";
+		config.title = "Team3";
 		config.width = 1280/2;
 		config.height = 720/2;
 		config.fullscreen = false;
 		config.vSyncEnabled = false;
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new Team3(), config);
 	}
 }
