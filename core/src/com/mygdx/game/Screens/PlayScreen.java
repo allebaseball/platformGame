62e48a95ca96f04219c5b794dfabacbf169951ca
Waddafac <castellini.davide19@gmail.com>
2015-11-25 11:56:29 +0100

    Added classes

---
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -0,0 +1,63 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.Scenes.Hud;
+import com.mygdx.game.Team3;
+
+public class PlayScreen implements Screen {
+    private Team3 game;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
+
+    public PlayScreen(Team3 game) {
+        this.game = game;
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(Team3.V_WIDTH, Team3.V_HEIGHT, gamecam);
+        hud = new Hud(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/mygdx/game/Team3.java
@@ -0,0 +1,25 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screens.PlayScreen;
+
+public class Team3 extends Game {
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+
+
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+}
