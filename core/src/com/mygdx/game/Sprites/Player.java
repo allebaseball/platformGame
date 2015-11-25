0475ce3ddf316a3d0557ab6ae02bb482a92c2b06
Waddafac <castellini.davide19@gmail.com>
2015-11-25 15:58:49 +0100

    Moved Player class

---
+++ core/src/com/mygdx/game/Sprites/Player.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.Sprites;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2s.world;
+
+public class Player extends Sprite {
+    public World world
+    public body b2body;
+
+
+}
