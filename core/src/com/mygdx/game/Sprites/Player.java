37a868f9f5c10a08c88baece46fef156c6cb1d3f
Waddafac <castellini.davide19@gmail.com>
2015-11-26 19:45:25 +0100

    Added player, bodies and fixtures

--- core/src/com/mygdx/game/Sprites/Player.java
+++ core/src/com/mygdx/game/Sprites/Player.java
@@ -1,11 +1,32 @@
 package com.mygdx.game.Sprites;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.physics.box2s.world;
+import com.badlogic.gdx.physics.box2d.*;
+import com.mygdx.game.Team3;
 
 public class Player extends Sprite {
-    public World world
-    public body b2body;
+    public World world;
+    public Body b2body;
 
+    public BodyDef bdef;
+
+    public Player(World world) {
+        this.world = world;
+        definePlayer();
+    }
+
+    public void definePlayer() {
+        bdef = new BodyDef();
+        bdef.position.set(30 / Team3.PPM, 300 / Team3.PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(10 / Team3.PPM);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
+    }
 
 }
